92bc9dda16f5194ade5961fe91d9a29b43f0f051
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-10 16:07:19 -0500

    FIXED

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,66 @@
 "use strict"
 console.log("hello")
 
-// these are our teams
+//these are our teams
 let teams = [
     {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
     {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
     {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
-    {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},   
+    {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},
 ];
 
-// this is our windows.onload function that runs code once the dom is loaded
+//this is our windows.onload function that runs code once the dom is loaded
 window.onload = function(){
 
     initDropdown();
 
-    // grab out button off the page
+    //grab our button off the page
     let theButton = document.querySelector("#theButton");
 
-    // when our button is clicked call displayFootballTeam
+    //when our button is clicked call displayFootballTeam
     theButton.addEventListener("click", displayFootballTeam);
 
 }
 
-// this function will display the details about out football team
+//this function will display the details about our football team
 function displayFootballTeam(){
 
-    // get the dropdown from the page
+    //get the dropdown from the page
     let theDropdown = document.querySelector("#footballSelect");
-    // get the dropdown from the page
+    //get the dropdown from the page
     let resultsParagraph = document.querySelector("#results");
 
-    // get the index of the selected option in the dropdown
+    //get the index of the selected option in the dropdown
     let selectedIndex = theDropdown.selectedIndex;
-
-    // get the team out of the teams array based on the index from the dropdown
+    //get the team out of the teams array based on the index from the dropdown
     let selectedTeam = teams[selectedIndex];
+    //put the info about the selected team in our paragraph
+    resultsParagraph.innerHTML = `You selected the ${selectedTeam.name} (${selectedTeam.code}) who play in ${selectedTeam.plays}`
 
-    // put the info about the selected team in our paragraph
-    resultsParagraph.innerHTML = `You selected the ${selectedTeam.name} (${selectedTeam.code}) who plays in ${selectedTeam.plays}`
 
 }
 
 function initDropdown(){
 
-    // get the dropdown from the HTML document and assign it to a variable
+    //get the dropdown from the HTML document and assign it to a variable
     let theDropdown = document.querySelector("#footballSelect");
 
-    // get the total number of teams we have for use in the loop
+    //get the total number of teams we have for use in the loop
     let numberOfTeams = teams.length;
 
-    // lets start looping over the teams
-    for(let i = 0; i < numberOfTeams; i++){
+    //lets start looping over the teams
+    for(let i=0; i < numberOfTeams; i++){
 
-        // lets create a new option
+        //lets create a new option
         let newOption = document.createElement("option");
 
-        // set the textContent for our new option, what the user sees
+        //set the textContent for our new option, what the user sees
         newOption.textContent = teams[i].name
 
-        // set the value for the option
+        //set the value for the option
         newOption.value = teams[i].code
 
-        // add this option to the dropdown
+        //add this option to the dropdown
         theDropdown.appendChild(newOption);
 
     }
